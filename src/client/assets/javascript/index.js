4985c8fd7a8d23f7ce5e61dc40184d26444c7926
robcrockPI <robert@propellerindustries.com>
2020-09-11 14:03:38 -0300

    Fixed the Start Race function.

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -387,7 +387,7 @@ function startRace(id) {
     method: 'POST',
     ...defaultFetchOpts(),
   })
-    .then(res => res.json())
+    // .then(res => res.json())
     .catch(err => console.log('Problem with getRace request::', err));
 }
 
